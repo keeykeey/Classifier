60d62c6c25d87fbe9e3785f55e4d56d94b256bfa
keeykeey <sh1n_fuj1to@icloud.com>
2020-06-09 22:10:06 +0900

    write model.py

--- model.py
+++ model.py
@@ -63,11 +63,11 @@ class Classifier():
                  optimizer=OPTIMIZER,
                  metrics = ['accuracy'])
 
-        model.fit(self.trainTrain,self.trainTrainLabel)
+        model.fit(self.trainTrain,self.trainTrainLabel,batch_size=32,epochs=EPOCHS)
 
         return model    
 
-    def eval():
+    def eval(model):
         pred = model.predict(self.trainValid)     
         pred = np.argmax(pred,axis = 1)
         trueAnswer = np.argmax(self.trainValidLabel,num_classes)
@@ -76,19 +76,26 @@ class Classifier():
         print('precision:',precision)
 
 def main():
-    _dogs = np.load("ResizedDogImg.npy")
-    _cats = np.load("ResizedCatImg.npy")
-    _dogsLabels = np.array([ 1 for i in range(len(_dogs))])
-    _catsLabels = np.array([ 0 for i in range(len(_cats))])
+    rangeIndex = [np.random.randint(0,4000,400)]
+    _dogs = np.load("ResizedDogImg.npy")[:400][rangeIndex]
+    _cats = np.load("ResizedCatImg.npy")[:400][rangeIndex]
+    _dogsLabel = np.array([ 1 for i in range(len(_dogs))])
+    _catsLabel = np.array([ 0 for i in range(len(_cats))])
 
     _dogsCats = np.append(_dogs,_cats)
-    _dogsCatsLabels =np.append(_dogsLabels,_catsLabels)
+    _dogsCatsLabel =np.append(_dogsLabels,_catsLabels)
 
-    Classifier = Classifier(ImageData,LabelData)
-    Classifier.train()    #学習
+    trainTrain = _dogsCats[:300]
+    trainValid = _dogsCats[300:]
+    trainTrainLabel = _dogsCatsLabel[:300]
+    trainValidLabel = _dogsCatsLabel[300:]
+
+    Classifier = Classifier(trainTrain,trainTrainLabel,trainValid,trainValidLabel)
+    model = Classifier.train()    #学習
     Classifier.eval()     #評価
 
-
+if __name__ == '__main__':
+    main()
 
 
 
